2c4c0031185a3a3b01c4aeba29d2ae6b64c93eeb
thidantas <eng.thiagohpdantas@gmail.com>
2025-05-01 20:53:12 -0300

    chore(lint): add ESLint and Prettier configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,69 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import react from 'eslint-plugin-react'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default defineConfig([
+  {
+    extends: compat.extends(
+      'next/core-web-vitals',
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'prettier',
+      'plugin:storybook/recommended'
+    ),
 
-export default eslintConfig;
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      react
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'import/no-anonymous-default-export': [
+        'error',
+        {
+          allowArray: true,
+          allowArrowFunction: true,
+          allowAnonymousClass: true,
+          allowAnonymousFunction: true,
+          allowCallExpression: true, // The true value here is for backward compatibility
+          allowNew: true,
+          allowLiteral: true,
+          allowObject: true
+        }
+      ],
+      '@typescript-eslint/no-unused-vars': 'warn'
+    },
+
+    ignores: ['.storybook', '.jest', 'generators']
+  }
+])
