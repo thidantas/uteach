28e5087aae62329abd55bed2ac44382ed3cfacac
thidantas <eng.thiagohpdantas@gmail.com>
2025-05-02 14:39:36 -0300

    refactor(arrow-right): rename file and update imports accordingly

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import ArrowRightIcon from '@/assets/icons/ArrowRight'
+import ArrowRightIcon from 'assets/icons/ArrowRight'
 
 import Button from '.'
 
