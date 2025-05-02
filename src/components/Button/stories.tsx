0b9675dcf35184d6e0c026cd2a846d122f752bc9
thidantas <eng.thiagohpdantas@gmail.com>
2025-05-02 12:43:56 -0300

    feat(button): create Storybook file to document Button component variants

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,54 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import ArrowRightIcon from 'assets/icons/arrowRight'
+
+import Button from '.'
+
+export default {
+  title: 'components/Button',
+  component: Button,
+  argsTypes: {
+    children: {
+      type: 'string'
+    },
+    size: {
+      options: ['small', 'medium', 'large'],
+      control: { type: 'inline-radio' }
+    },
+    color: {
+      options: ['primary', 'secondary', 'outlined'],
+      control: { type: 'inline-radio' }
+    },
+    bold: {
+      control: { type: 'boolean' }
+    },
+    position: {
+      options: ['right', 'left'],
+      control: { type: 'inline-radio' }
+    }
+  }
+} as Meta
+
+export const Default: StoryObj = {
+  args: {
+    children: 'Sign Up Now'
+  }
+}
+
+export const WithIcon: StoryObj = {
+  args: {
+    children: 'Sign Up Now',
+    icon: <ArrowRightIcon />,
+    position: 'right'
+  }
+}
+
+export const AsLink: StoryObj = {
+  args: {
+    children: 'Find more about the app',
+    icon: <ArrowRightIcon />,
+    as: 'a',
+    href: '#',
+    minimal: true
+  }
+}
