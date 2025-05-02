b739efbefa5054c4db39aeb47f4d1d03a86063a8
thidantas <eng.thiagohpdantas@gmail.com>
2025-05-01 21:12:35 -0300

    chore(storybook): configure Storybook for component development

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import path from 'path'
+import { StorybookConfig } from '@storybook/experimental-nextjs-vite'
+
+const config: StorybookConfig = {
+  staticDirs: ['../public'],
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/experimental-nextjs-vite',
+    options: {}
+  },
+  docs: {},
+  viteFinal: async (config) => {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = {
+      ...(config.resolve?.alias || {}),
+      components: path.resolve(__dirname, '../src/components'),
+      styles: path.resolve(__dirname, '../src/styles')
+    }
+    return config
+  }
+}
+
+export default config
---
+++ .storybook/preview.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyles from '../src/styles/globals'
+import theme from '../src/styles/theme'
+
+export const parameters = {
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white
+      },
+      {
+        name: 'dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+}
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles removeBg />
+      <Story />
+    </ThemeProvider>
+  )
+]
+export const tags = ['autodocs']
