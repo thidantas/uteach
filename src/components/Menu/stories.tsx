b65e0a8ba3ac199c881e96db39cd919ce9d1fbd4
thidantas <eng.thiagohpdantas@gmail.com>
2025-05-02 14:40:45 -0300

    feat(menu): add stories file for Menu component

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Menu from '.'
+
+export default {
+  title: 'components/Menu',
+  component: Menu
+} as Meta
+
+export const Default: StoryObj = {}
