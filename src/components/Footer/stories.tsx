0454f19cbdc224a918c1163df19d5539c0407ff9
thidantas <eng.thiagohpdantas@gmail.com>
2025-05-02 23:31:07 -0300

    feat(footer): add stories file for Footer component

---
+++ src/components/Footer/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Footer from '.'
+
+export default {
+  title: 'components/Footer',
+  component: Footer
+} as Meta
+
+export const Default: StoryObj = {}
