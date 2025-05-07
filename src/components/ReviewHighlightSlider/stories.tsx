028b05dddc7a0685f88184b3113a9708088d2b8c
thidantas <eng.thiagohpdantas@gmail.com>
2025-05-06 22:04:38 -0300

    feat(review-highlight-slider): create ReviewHighlightSlider component

---
+++ src/components/ReviewHighlightSlider/stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import ReviewHighlightSlider from '.'
+
+export default {
+  title: 'ReviewHighlightSlider',
+  component: ReviewHighlightSlider
+} as Meta
+
+export const Default: StoryObj = {}
