f8a3875627d31dcfec0088438140b52d48567bd0
thidantas <eng.thiagohpdantas@gmail.com>
2025-05-07 18:57:24 -0300

    chore(assets): add new images to public folder and a new icon

---
+++ src/assets/icons/NavDrawer.tsx
@@ -0,0 +1,21 @@
+import { SvgIconProps } from 'types/types'
+
+const NavDrawerIcon = (props: SvgIconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="25"
+    fill="none"
+    viewBox="0 0 24 25"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M3 8a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 8M3 12.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12.5M3 17a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 17"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+)
+
+export default NavDrawerIcon
